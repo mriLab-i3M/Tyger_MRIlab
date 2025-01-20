505fc5dd551ab882b547ed294a3657b941ecc622
TeresaGuallartNaval <teresaguallartnaval@hotmail.com>
2025-01-20 19:33:01 +0100

    TGN - MRD to 3D

---
+++ 3D/fromMRDtoMAT3D.py
@@ -0,0 +1,35 @@
+import sys
+import argparse
+import mrd
+import scipy.io as sio
+import numpy as np
+
+def export(input, output):
+    with mrd.BinaryMrdReader(input) as r:
+        header = r.read_header()
+        for item in r.read_data():
+            if not isinstance(item, mrd.StreamItem.ImageFloat):
+                raise RuntimeError("Stream must contain only floating point images")
+
+            img = item.value
+            imgRecon = img.data
+            
+    rawData = sio.loadmat(output)
+    rawData['imgReconTyger3D_20.01.25'] = imgRecon
+    sio.savemat(output, rawData)
+            
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Save recon in .mat file")
+    parser.add_argument('-i', '--input', type=str, required=False,
+                        help="Input file (default stdin)")
+    parser.add_argument('-o', '--output', type=str, required=False,
+                        help="Output filename .mat")
+    # parser.set_defaults(
+    #     input = '/home/teresa/marcos_tyger/Tyger_MRIlab/3D/testResult.bin',
+    #     output= '/home/teresa/marcos_tyger/Tyger_MRIlab/3D/knee.mat',
+    # )
+    args = parser.parse_args()
+
+    input = open(args.input, "rb") if args.input is not None else sys.stdin.buffer
+
+    export(input, args.output)
